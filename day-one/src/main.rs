6070dcc9229be1c0b919a3a73e4b1ea255aa73f9
Mike Bruce <mikepjb@proton.me>
2024-07-10 16:01:52 +0100

    neither part is correct, but close..

--- day-one/src/main.rs
+++ day-one/src/main.rs
@@ -1,20 +1,72 @@
-// use std::fs;
+use std::fs;
 use std::error::Error;
 
+const NUMERICAL_VALUES: [(&str, &str); 9] = [
+    ("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5"),
+    ("6", "6"), ("7", "7"), ("8", "8"), ("9", "9")
+];
+
+const TEXT_VALUES: [(&str, &str); 9] = [
+    ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"),
+    ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")
+];
+
 fn main() -> Result<(), Box<dyn Error>> {
-    // let day_input = fs::read_to_string("./input/day-one.txt")?;
-    // println!("{}", part_one(day_input));
-    calibration_values("hello".to_string());
-    part_one("hello".to_string());
+    let day_input = fs::read_to_string("./input/day-one.txt")?;
+
+    let mut all_values = Vec::new();
+    all_values.extend_from_slice(&TEXT_VALUES);
+    all_values.extend_from_slice(&NUMERICAL_VALUES);
+
+    println!("{}", part_one(day_input.clone()));
+    println!("{}", part_two(day_input.clone()));
     Ok(())
 }
 
-fn part_one(_input: String) -> i32 {
-    32
+fn part_one(input: String) -> i32 {
+    let mut values = Vec::new();
+    values.extend_from_slice(&NUMERICAL_VALUES);
+    sum_lines(input, values)
 }
 
-fn calibration_values(_line: String) -> i32 {
-    32
+fn part_two(input: String) -> i32 {
+    let mut values = Vec::new();
+    values.extend_from_slice(&NUMERICAL_VALUES);
+    values.extend_from_slice(&TEXT_VALUES);
+    sum_lines(input, values)
+}
+
+fn sum_lines(input: String, values: Vec<(&str, &str)>) -> i32 {
+    input.lines()
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|line| calibration_values(line.to_string(), &values))
+        .sum::<i32>()
+}
+
+fn calibration_values(line: String, values: &Vec<(&str, &str)>) -> i32 {
+    // find index for all values.. pick the largest and smallest indexes and use their values
+    let mut maxt: (i32, &str) = (-1, "");
+    let mut mint: (i32, &str) = (-1, "");
+
+    for (m, v) in values {
+        let (max, min) = (maxt.0, mint.0);
+        if let Some(i) = line.find(m) {
+            let index = i.try_into().unwrap(); // from unsigned to signed
+            if index < min || min == -1 {
+                mint = (index, v)
+            }
+
+            if index > max {
+                maxt = (index, v)
+            }
+        }
+    }
+
+    println!("{}, {:?}, {:?}", line, mint, maxt);
+    let out = (mint.1.to_string() + &maxt.1.to_string()).to_string().parse::<i32>().unwrap();
+    println!("{}", out);
+    out
 }
 
 #[cfg(test)]
@@ -23,10 +75,19 @@ mod tests {
 
     #[test]
     fn test_single_line_part_one() {
-        assert_eq!(calibration_values("1abc2".to_string()), 12);
-        assert_eq!(calibration_values("2sdafs3".to_string()), 23);
-        assert_eq!(calibration_values("2sdaf3s".to_string()), 23);
-        assert_eq!(calibration_values("dfjnzxtlnine9five".to_string()), 95);
+        assert_eq!(calibration_values("1abc2".to_string(), &NUMERICAL_VALUES.to_vec()), 12);
+        assert_eq!(calibration_values("2sdafs3".to_string(), &NUMERICAL_VALUES.to_vec()), 23);
+    }
+
+    #[test]
+    fn test_single_line_part_two() {
+        let mut all_values = Vec::new();
+        all_values.extend_from_slice(&TEXT_VALUES);
+        all_values.extend_from_slice(&NUMERICAL_VALUES);
+
+        assert_eq!(calibration_values("dfjnzxtlnine9five".to_string(), &all_values), 95);
+        assert_eq!(calibration_values("eightwothree".to_string(), &all_values), 83);
+        assert_eq!(calibration_values("abcone2threexyz".to_string(), &all_values), 13);
     }
 
     #[test]
@@ -41,6 +102,7 @@ treb7uchet";
 
     #[test]
     fn test_example_part_two() {
+        println!("hello!");
         let example_input = "two1nine
 eightwothree
 abcone2threexyz
@@ -48,6 +110,6 @@ xtwone3four
 4nineeightseven2
 zoneight234
 7pqrstsixteen";
-        assert_eq!(part_one(example_input.to_string()), 281);
+        assert_eq!(part_two(example_input.to_string()), 281);
     }
 }
