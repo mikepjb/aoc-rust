8853c3cfadd764169ac58504410bc821b125a164
Mike Bruce <mikepjb@proton.me>
2024-07-11 17:01:59 +0100

    mini-test setup for day three

--- day-three/src/main.rs
+++ day-three/src/main.rs
@@ -5,12 +5,35 @@ fn main() -> Result<(), Box<dyn Error>>{
     Ok(())
 }
 
+fn engine_parts(engine_schematic: String) -> Vec<usize> {
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
     #[test]
     fn simple_test() {
         assert!(true)
+    }
+
+    #[test]
+    fn small_engine_test() {
+        let small_engine = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+        assert_eq!(
+            engine_parts(small_engine.to_string()),
+            vec![467, 35, 633, 617, 592, 755, 664, 598]
+        )
     }
 }
